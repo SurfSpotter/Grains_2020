a31f222ffac4f7923d5f15ba74ac3258a1714ca1
dur5t <mukasor@gmail.com>
2020-02-07 02:01:12 +0300

    Added image in description View Controller

--- Grains/DescriptionViewController.swift
+++ Grains/DescriptionViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class DescriptionViewController: UIViewController {
 
-    @IBOutlet weak var labelOutlet: UILabel!
-    
+    @IBOutlet weak var viewOutlet: UIView!  // Фон
+
     var descriptionGrainClass: Grain?
+    
+    @IBOutlet weak var grainMainImageOut: UIImageView!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = descriptionGrainClass?.name
-        
-
-    
+        viewOutlet.backgroundColor = descriptionGrainClass?.backgroundColour // цвет фона из класса крупы
+        setGrainImage()
 }
+    
+    
+    
+    // MARK: - Загружаем изображение для класса этой крупы
+    fileprivate func setGrainImage() {
+        
+        
+        
+        
+        
+        
+        
+        if descriptionGrainClass?.grainImgName != nil {
+            grainMainImageOut.image = UIImage(named: descriptionGrainClass!.grainImgName)
+        }
+        else {
+            print ("image not found!")
+        }
+    }
 }
