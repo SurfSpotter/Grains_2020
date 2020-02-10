809f86e2987e4e50113a55816cd3667f6c680a24
dur5t <mukasor@gmail.com>
2020-02-11 01:44:52 +0300

    calories View

--- Grains/DescriptionViewController.swift
+++ Grains/DescriptionViewController.swift
@@ -22,16 +22,37 @@ class DescriptionViewController: UIViewController {
     
     @IBOutlet weak var descLabelOutlet: UILabel!
     
-
+    @IBOutlet weak var timerButtonOut: UIButton!
+    
+    @IBOutlet weak var caloriesButtonOut: UIButton!
+    
+    @IBOutlet weak var boilTimeLabOut: UILabel!
+    
+    @IBOutlet weak var calLabOut: UILabel!
+    
+    
+    // calories Descriprtion
+    
+    @IBOutlet weak var carbLabOut: UILabel!
+    
+    @IBOutlet weak var fatLabOut: UILabel!
+    
+    @IBOutlet weak var protLabOut: UILabel!
     
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         navigationItem.title = descriptionGrainClass?.name
         descLabelOutlet.text = descriptionGrainClass?.description
         viewOutlet.backgroundColor = descriptionGrainClass?.backgroundColour // цвет фона из класса крупы
         setGrainImage()
+     
+        // Hide the labels in calories description Window
         
+        carbLabOut.isHidden = true
+        protLabOut.isHidden = true
+        fatLabOut.isHidden = true
         
         
         settingsCharactersViewOutlet()
@@ -42,6 +63,40 @@ class DescriptionViewController: UIViewController {
 //        self.layer.masksToBounds = false
 //        self.clipsToBounds = true
 }
+    
+    
+    @IBAction func setTimerAction(_ sender: Any) {
+        animationOfView(item: charactersViewOutlet)
+        
+    }
+    
+    
+    
+    // При нажатии на кнопку, переворачивется экран и показываются калории
+    
+    @IBAction func showCaloriesButton(_ sender: Any) {
+        self.animationOfView(item: self.charactersViewOutlet)
+        self.timerButtonOut.isHidden = true
+        self.caloriesButtonOut.isHidden = true
+        self.calLabOut.isHidden = true
+        self.boilTimeLabOut.isHidden = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0 )  {
+            
+            if (self.descriptionGrainClass) != nil {
+                self.carbLabOut.isHidden = false
+                self.carbLabOut.text = " Углеводов: \(String(describing: self.descriptionGrainClass!.carbohydrate) )"
+                self.protLabOut.isHidden = false
+                self.protLabOut.text = " Белков: \(String(describing: self.descriptionGrainClass!.protein) )"
+                self.fatLabOut.isHidden = false
+                self.fatLabOut.text = " Жиров: \(String(describing: self.descriptionGrainClass!.fat) )" }
+           
+        }
+        
+        
+       
+    }
+    
+    
     
     
     
@@ -80,5 +135,20 @@ class DescriptionViewController: UIViewController {
     override func viewWillLayoutSubviews() {
         descLabelOutlet.sizeToFit()
     }
-       
+    
+    func animationOfView(item: UIView ) {
+    let item = item
+    UIView.animate(withDuration: 0.6,
+                   animations: {
+                    item.transform = CGAffineTransform(scaleX: 0.01, y: 0.95)
+    },
+                   completion: { _ in
+                    UIView.animate(withDuration: 0.6) {
+                        item.transform = CGAffineTransform.identity
+                    }
+    })
+    
+    }
+    
+    
 }
